defb573f2d8dc911deae5c19de898259f7cad5c1
Shaheen nazar <101840286+Shaheenzr@users.noreply.github.com>
2022-10-16 19:02:37 -0700

    added link of web references  and removed comments

--- week4/CODE_LAB_4/consumer/code/consumer.py
+++ week4/CODE_LAB_4/consumer/code/consumer.py
@@ -4,25 +4,20 @@ import requests
 import os
 import json
 
+took reference from below resources
+# https://stackoverflow.com/questions/39579666/how-to-set-background-image-on-flask-templates
+# https://pythonbasics.org/flask-template-data/
+# https://python-adv-web-apps.readthedocs.io/en/latest/flask3.html
+# https://dzone.com/articles/build-deploy-flask-application-using-docker
+# https://towardsdatascience.com/a-series-on-flask-apis-part-1-getting-and-posting-33985dfe8816
 
 app = Flask(__name__)
 
 @app.route('/',methods= ['GET'])
 def index():
-   
-    #meal_item = response.text
-    #return render_template('index.html', food=meal_item)
-
-    #api_host = os.environ.get('API_HOST')
-    #api_port = os.environ.get('API_PORT')
-    #api_endpoint = os.environ.get('API_ENDPOINT')
-    #url = f'http://{api_host}:{api_port}/{api_endpoint}'
-
     response = requests.get('http://api:5000/meal')
     recommend = json.loads(response.text)
     return render_template('home.html',meal_name=recommend['foodName'],meal_price=recommend['price'])
-
-
 
 if __name__ == "__main__":
     
