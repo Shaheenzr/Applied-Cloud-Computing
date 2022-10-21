9b11b1ac80190e464d4d0cf5bf5877e5d0520c94
Shaheen nazar <101840286+Shaheenzr@users.noreply.github.com>
2022-10-21 14:26:40 -0700

    Add files via upload

---
+++ week5/db/init/init.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS Meals (
+  foodName TEXT NOT NULL,
+  price REAL NOT NULL
+);
+
+INSERT INTO Meals(foodName, price) VALUES ('Chicken korma',15.00);
+INSERT INTO Meals(foodName, price) VALUES ('Mongolian beef',16.00);
+INSERT INTO Meals(foodName, price) VALUES ('Hunan chicken',14.00);
+INSERT INTO Meals(foodName, price) VALUES ('Tomato soup',10.00);
+INSERT INTO Meals(foodName, price) VALUES ('Lamb cumin',18.50);
+INSERT INTO Meals(foodName, price) VALUES ('Bean Cheese Burrito',10.75);
+INSERT INTO Meals(foodName, price) VALUES ('Schezwan Fried rice',12.00);
+INSERT INTO Meals(foodName, price) VALUES ('Hakka noodles',14.00);
+INSERT INTO Meals(foodName, price) VALUES ('Margarita Pizza',8.00);
+INSERT INTO Meals(foodName, price) VALUES ('Impossible Burger',7.50);
+INSERT INTO Meals(foodName, price) VALUES ('Chicken Ramen',14.75);
+INSERT INTO Meals(foodName, price) VALUES ('Vegetarian Tacos',4.54);
+INSERT INTO Meals(foodName, price) VALUES ('Vegan Thai curry',12.56);
+INSERT INTO Meals(foodName, price) VALUES ('White Rice',3.50);
+INSERT INTO Meals(foodName, price) VALUES ('Sauerbraten',14.50);
+INSERT INTO Meals(foodName, price) VALUES ('Gulasch',16.75);
